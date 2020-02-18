5d4027b7929f5eb818075a1ed3647b024aad161c
NemanjaMilenkovic <nemanja.milenkovic@gmail.com>
2020-02-18 17:06:29 +0900

    Added axios, endpoint.

--- world-corona-stats/server/index.js
+++ world-corona-stats/server/index.js
@@ -7,6 +7,21 @@ app.listen(port, () => console.log(`Listening on Port ${port}`));
 
 // api here
 
-app.get("/api/people", async (req, res) => {});
+const coronaData = axios({
+  method: "GET",
+  url:
+    "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
+  headers: {
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+    "x-rapidapi-key": "130e574162msh6cc16dbd234fc6ep18d4e3jsn9a2bc8ca2759"
+  }
+})
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-module.exports = app;
+module.exports = { app };
